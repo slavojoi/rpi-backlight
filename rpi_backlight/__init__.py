fe31ae547f8c8912746e49f2e413db3337ae4f82
slavojoi <43029766+slavojoi@users.noreply.github.com>
2022-11-15 22:57:39 +0100

    Update __init__.py

--- rpi_backlight/__init__.py
+++ rpi_backlight/__init__.py
@@ -31,7 +31,7 @@ _BACKLIGHT_SYSFS_PATHS = {
     BoardType.RASPBERRY_PI: (
         "/sys/class/backlight/10-0045/"
         if Path("/sys/class/backlight/10-0045/").exists()
-        else "/sys/class/backlight/rpi_backlight/"
+        else "/sys/waveshare/rpi_backlight/brightness"
     ),
     BoardType.TINKER_BOARD: "/sys/devices/platform/ff150000.i2c/i2c-3/3-0045/",
     BoardType.TINKER_BOARD_2: "/sys/devices/platform/ff3e0000.i2c/i2c-8/8-0045/",
